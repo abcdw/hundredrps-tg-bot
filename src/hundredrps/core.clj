1d42945cf3bea1aabab36d4ca0c053bd3f1228f7
Andrew Tropin <andrew@trop.in>
2022-10-26 19:08:46 +0400

    Add echo functionality

--- src/hundredrps/core.clj
+++ src/hundredrps/core.clj
@@ -1,10 +1,12 @@
 (ns hundredrps.core
   (:gen-class)
   (:require
-   [org.httpkit.server :as http-kit]
    [aero.core :as aero]
    [clojure.java.io :as io]
-   [integrant.core :as ig]))
+   [integrant.core :as ig]
+   [jsonista.core :as j]
+   [org.httpkit.client :as http]
+   [org.httpkit.server :as http-kit]))
 
 (defn -main
   "Entry point."
@@ -32,11 +34,22 @@
 (defmethod ig/halt-key! :http/server [_ server]
   (http-kit/server-stop! server))
 
-  (fn [_]
-    {:status  200
-     :headers {"Content-Type" "text/plain"}
-     :body    (str "Huey " name)}))
 (defmethod ig/init-key :handler/webhook [_ {:keys [api-token]}]
+  (fn [{:keys [body]}]
+    (let [input (j/read-value body j/keyword-keys-object-mapper)
+          {{{chat-id :id} :chat
+            text          :text} :message} input
+          reply-msg '()]
+
+      (->
+       @(http/post
+         (str "https://api.telegram.org/bot" api-token "/sendMessage")
+         {:form-params {:chat_id chat-id
+                        :text    text}})
+       println)
+      {:status  200
+       :headers {"Content-Type" "text/plain"}
+       :body    (str "Hey")})))
 
 (defn get-config
   "Read integrant system description from config.edn."
