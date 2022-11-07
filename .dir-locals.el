b7dc15efea8ceba4f1b8159335e4d8af2ee5d59f
Andrew Tropin <andrew@trop.in>
2022-11-07 09:46:01 +0400

    .dir-locals.el: Add reset binding to for cider-repl as well

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
-((clojure-mode . ((eval . (define-key clojure-mode-map (kbd "s-r")
-                                      (lambda () (interactive)
-                                        (cider-interactive-eval "(integrant.repl/reset)"))))
-                  (cider-preferred-build-tool . clojure-cli)
-                  (cider-clojure-cli-aliases . ":dev:test"))))
+((cider-repl-mode
+  . ((eval . (define-key cider-repl-mode-map (kbd "s-r")
+                         (lambda () (interactive)
+                           (cider-interactive-eval "(integrant.repl/reset)"))))))
+ (clojure-mode
+  . ((eval . (define-key clojure-mode-map (kbd "s-r")
+                         (lambda () (interactive)
+                           (cider-interactive-eval "(integrant.repl/reset)"))))
+     (cider-preferred-build-tool . clojure-cli)
+     (cider-clojure-cli-aliases . ":dev:test"))))
