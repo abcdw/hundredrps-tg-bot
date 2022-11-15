b93390ae91dc38fe6641a225d4816c5b29c82ea0
Andrew Tropin <andrew@trop.in>
2022-11-15 16:55:43 +0400

    .dir-locals.el: Use user/reset to change reload behavior

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,10 @@
 ((cider-repl-mode
   . ((eval . (define-key cider-repl-mode-map (kbd "s-r")
                          (lambda () (interactive)
-                           (cider-interactive-eval "(integrant.repl/reset)"))))))
+                           (cider-interactive-eval "(user/reset)"))))))
  (clojure-mode
   . ((eval . (define-key clojure-mode-map (kbd "s-r")
                          (lambda () (interactive)
-                           (cider-interactive-eval "(integrant.repl/reset)"))))
+                           (cider-interactive-eval "(user/reset)"))))
      (cider-preferred-build-tool . clojure-cli)
      (cider-clojure-cli-aliases . ":dev:test:build"))))
