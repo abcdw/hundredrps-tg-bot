cda4cf0a54b1f851ef45d8e48d6c98ce859d0faa
Andrew Tropin <andrew@trop.in>
2022-11-18 11:07:19 +0400

    Add image ns with crop-img and vertical? fns

---
+++ src/hundredrps/image.clj
@@ -0,0 +1,47 @@
+(ns hundredrps.image
+  (:require [clojure.java.io :as io])
+  (:import javax.imageio.ImageIO
+           java.io.ByteArrayOutputStream))
+
+(defn vertical?
+  [img]
+  (with-open [img (. ImageIO read (io/input-stream img))]
+    (let [height (.getHeight img)
+          width  (.getWidth img)]
+      (if (< width height)
+        true
+        false))))
+
+(defn crop-img
+  [img aspect-ratio]
+  (with-open [img    (. ImageIO read (io/input-stream img))
+              buffer (new ByteArrayOutputStream)]
+    (let [target-ratio  aspect-ratio
+          width         (.getWidth img)
+          height        (.getHeight img)
+          current-ratio (/ width height)
+
+          new-width (if (< target-ratio current-ratio)
+                      (int (* target-ratio height))
+                      width)
+          x         (int (/ (- width new-width) 2))
+
+          new-height  (if (< target-ratio current-ratio)
+                        height
+                        (int (/ width target-ratio)))
+          y           (int (/ (- height new-height) 2))
+          cropped-img (.getSubimage img x y new-width new-height)]
+
+      (. ImageIO write cropped-img "jpg" buffer)
+      (.toByteArray buffer))))
+
+(comment
+  (def img-input-stream
+    (io/input-stream
+     (io/file (io/resource "face.jpg"))))
+
+  (def cropped-img
+    (crop-img img-input-stream (/ 16 9)))
+
+  (with-open [os (io/output-stream (io/file "resources/cropped.jpg"))]
+    (.write os cropped-img)))
