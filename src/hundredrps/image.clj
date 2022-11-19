3117144fdd11d50a563d46a53774a91c4d843ffb
Andrew Tropin <andrew@trop.in>
2022-11-19 18:45:37 +0400

    image: Remove unecessary with-open
    read from ImageIO closes the stream

--- src/hundredrps/image.clj
+++ src/hundredrps/image.clj
@@ -5,33 +5,32 @@
 
 (defn vertical?
   [img]
-  (with-open [img (. ImageIO read (io/input-stream img))]
-    (let [height (.getHeight img)
-          width  (.getWidth img)]
-      (if (< width height)
-        true
-        false))))
+  (let [img    (. ImageIO read (io/input-stream img))
+        height (.getHeight img)
+        width  (.getWidth img)]
+    (if (< width height)
+      true
+      false)))
 
 (defn crop-img
   [img aspect-ratio]
-  (with-open [img    (. ImageIO read (io/input-stream img))
-              buffer (new ByteArrayOutputStream)]
-    (let [target-ratio  aspect-ratio
-          width         (.getWidth img)
-          height        (.getHeight img)
-          current-ratio (/ width height)
+  (let [img           (. ImageIO read (io/input-stream img))
+        target-ratio  aspect-ratio
+        width         (.getWidth img)
+        height        (.getHeight img)
+        current-ratio (/ width height)
 
-          new-width (if (< target-ratio current-ratio)
-                      (int (* target-ratio height))
-                      width)
-          x         (int (/ (- width new-width) 2))
+        new-width (if (< target-ratio current-ratio)
+                    (int (* target-ratio height))
+                    width)
+        x         (int (/ (- width new-width) 2))
 
-          new-height  (if (< target-ratio current-ratio)
-                        height
-                        (int (/ width target-ratio)))
-          y           (int (/ (- height new-height) 2))
-          cropped-img (.getSubimage img x y new-width new-height)]
-
+        new-height  (if (< target-ratio current-ratio)
+                      height
+                      (int (/ width target-ratio)))
+        y           (int (/ (- height new-height) 2))
+        cropped-img (.getSubimage img x y new-width new-height)]
+    (with-open [buffer (new ByteArrayOutputStream)]
       (. ImageIO write cropped-img "jpg" buffer)
       (.toByteArray buffer))))
 
