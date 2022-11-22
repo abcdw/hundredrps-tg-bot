04d532726adf69603df8c235e96644d051979993
Andrew Tropin <andrew@trop.in>
2022-11-22 13:10:37 +0400

    text: Add remove-some-unicode-symbols fn

--- src/hundredrps/text.clj
+++ src/hundredrps/text.clj
@@ -1,6 +1,7 @@
 (ns hundredrps.text
   (:require [clojure.java.io :as io]
-            [malli.core :as m]))
+            [malli.core :as m])
+  (:import (java.util.regex Pattern)))
 
 ;; (defmethod ig/init-key :text/templates [_ val]
 ;;   val)
@@ -31,3 +32,40 @@
     :strings        (get-in system [:cards/resources :strings card])
     :text-templates (get-in system [:text/templates card])}
    eval-templates))
+
+(defn remove-some-unicode-symbols
+  [s]
+  (let [regex (Pattern/compile
+               (str
+                "["
+                "\uD83D\uDE00-\uD83D\uDE4F" ; emoticons
+                ;; "\U0001F600-\U0001F64F"
+
+                ;; TODO: Convert the rest with
+                ;; https://codepoints.net/U+1F64F?lang=en
+                ;; "\U0001F300-\U0001F5FF"  ; symbols & pictographs
+                ;; "\U0001F680-\U0001F6FF"  ; transport & map symbols
+                ;; "\U0001F1E0-\U0001F1FF"  ; flags (iOS)
+                ;; "\U00002500-\U00002BEF"  ; chinese char
+                ;; "\U00002702-\U000027B0"
+                ;; "\U00002702-\U000027B0"
+                ;; "\U000024C2-\U0001F251"
+                ;; "\U0001f926-\U0001f937"
+                ;; "\U00010000-\U0010ffff"
+                ;; "\u2640-\u2642"
+                ;; "\u2600-\u2B55"
+                ;; "\u200d"
+                ;; "\u23cf"
+                ;; "\u23e9"
+                ;; "\u231a"
+                ;; "\ufe0f"  ; dingbats
+                ;; "\u3030"
+                ;; "\U00002702-\U000027B0"
+                ;; "\U000024C2-\U0001F251"
+                ;; "\U0001f926-\U0001f937"
+                ;; "\u200d"
+                ;; "\u2640-\u2642"
+                "]+"))]
+    (clojure.string/replace s regex "")))
+
+;; (remove-some-unicode-symbols "test 😃")
