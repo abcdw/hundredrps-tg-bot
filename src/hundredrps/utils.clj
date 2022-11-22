13b5861efeec8ee78e6de5840c1d0a3b43ea9bfe
Andrew Tropin <andrew@trop.in>
2022-11-22 15:19:42 +0400

    utils: Accept both File and InputStream

--- src/hundredrps/utils.clj
+++ src/hundredrps/utils.clj
@@ -1,10 +1,13 @@
 (ns hundredrps.utils
   (:require [clojure.java.io :as io])
-  (:import java.io.ByteArrayOutputStream))
+  (:import java.io.ByteArrayOutputStream
+           java.io.InputStream))
 
 (defn load-file-as-byte-array
   [file]
-  (with-open [xin  (io/input-stream file)
+  (with-open [xin  (if (instance? InputStream file)
+                     file
+                     (io/input-stream file))
               xout (new ByteArrayOutputStream)]
     (io/copy xin xout)
     (.toByteArray xout)))
